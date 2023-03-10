1429a78be988ab02b6dfdd0c12591b2f5558b400
Nitish K <kaflenit@gmail.com>
2023-03-09 23:03:39 -0600

    update react script.js

--- react/script.js
+++ react/script.js
@@ -1,13 +1,15 @@
-
-<main>
-  <section className='container'>
-    <h1>Hello World</h1>
-  </section>
-  <section className="footer">
-    <h2>This is a footer</h2>
-  </section>
-</main>
-
+function Hello() {
+  return (
+    <main>
+      <section className='container'>
+        <h1>Hello World</h1>
+      </section>
+      <section className="footer">
+        <h2>This is a footer</h2>
+      </section>
+    </main>
+  )
+}
 
 
 
